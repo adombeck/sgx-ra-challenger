05055236f8af21403c94b14042bf2113c0d63b7a
Adrian Dombeck <adrian.dombeck@rub.de>
2017-05-09 15:26:27 +0200

    Use exceptions instead of assert statements

--- sgx_attester/util.py
+++ sgx_attester/util.py
@@ -1,5 +1,7 @@
 import os
 
+from sgx_attester.exceptions import UnexpectedLengthError
+
 from sgx_attester.config import KEY_DIR
 
 
@@ -13,3 +15,11 @@ def get_public_key():
     pubkey_path = os.path.join(KEY_DIR, "attester_public.key")
     with open(pubkey_path) as pubkey_file:
         return bytes.fromhex(pubkey_file.read())
+
+
+def check_json(data, type_, length=None):
+    if not isinstance(data, type_):
+        raise TypeError("Received JSON is of type %r, expected type %r" % (type(data), type_))
+
+    if length and len(data) != length:
+        raise UnexpectedLengthError("Received JSON %r has length %r, expected length %r" % (type_, data, length))
