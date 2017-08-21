b4c3e609becee2b41aa4a121be6bc869fd51e415
Adrian Dombeck <adrian.dombeck@rub.de>
2017-08-21 16:36:56 +0200

    Skip IAS interaction

--- sgx_ra_challenger/ias.py
+++ sgx_ra_challenger/ias.py
@@ -12,17 +12,7 @@ from sgx_ra_challenger import crypto
 
 
 def retrieve_sigrl(epid_group_id: bytes):
-    url = "https://%s:%s/attestation/sgx/v2/sigrl/%s" % (IAS_HOST, IAS_PORT, epid_group_id.hex())
-
-    response = requests.get(url, cert=(SSL_CERT_PATH, SSL_KEY_PATH))
-
-    logging.info("Retrieved signature revocation list from IAS in %s", response.elapsed)
-
-    if response.status_code != 200:
-        response.raise_for_status()
-
-    logging.info("response body: %r", response.content)
-    return base64.b64decode(response.content, validate=True)
+    return bytes()
 
 
 def get_nonce() -> str:
@@ -30,49 +20,12 @@ def get_nonce() -> str:
 
 
 def verify_quote(quote: bytes):
-    url = "https://%s:%s/attestation/sgx/v2/report" % (IAS_HOST, IAS_PORT)
-    nonce = get_nonce()
-
-    encoded_quote = base64.b64encode(quote).decode()
-
-    body = {
-        "isvEnclaveQuote": encoded_quote,
-        "nonce": nonce
-    }
-
-    response = requests.post(url, json=body, cert=(SSL_CERT_PATH, SSL_KEY_PATH))
-
-    logging.info("Retrieved verification report from IAS in %s", response.elapsed)
-
-    if response.status_code != 201:
-        response.raise_for_status()
-
-    report = response.json()
-    logging.debug("response headers: %r", response.headers)
-    logging.debug("verification report: %r", report)
-
-    if report["isvEnclaveQuoteStatus"] != "OK":
-        raise QuoteVerificationError("IAS returned quote status %r", report["isvEnclaveQuoteStatus"])
-
-    if not hmac.compare_digest(nonce, report["nonce"]):
-        raise QuoteVerificationError("IAS returned incorrect nonce")
-
-    verify_ias_response_signature(response)
-
-    verify_ias_response_quote(report, encoded_quote)
-
-    if not "epidPseudonym" in report:
-        return report["id"]
-    else:
-        return report["id"], report["epidPseudonym"]
+    return
 
 
 def verify_ias_response_signature(response):
-    signature = base64.b64decode(response.headers["x-iasreport-signature"])
-    with open(IAS_PUBKEY_PATH, 'br') as f:
-        pubkey = f.read()
-    crypto.verify_ias_signature(signature, pubkey, response.content)
+    return
 
 
 def verify_ias_response_quote(response_body, encoded_quote):
-    return hmac.compare_digest(response_body["isvEnclaveQuoteBody"], encoded_quote)
+    return
